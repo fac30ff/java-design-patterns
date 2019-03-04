f8b1b20978643c3bac407221f90f0342ac82865a
Igor Rudenko <ihor.rudenko@globallogic.com>
2019-03-04 14:20:59 +0200

    add decorator

---
+++ structural/decorator/src/test/java/com/github/fac30ff/decorator/exercise/Evaluate.java
@@ -0,0 +1,19 @@
+package com.github.fac30ff.decorator.exercise;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Evaluate {
+    @Test
+    public void test() {
+        Dragon dragon = new Dragon();
+
+        assertEquals("flying", dragon.fly());
+        assertEquals("too young", dragon.crawl());
+
+        dragon.setAge(20);
+
+        assertEquals("too old", dragon.fly());
+        assertEquals("crawling", dragon.crawl());
+    }
+}
